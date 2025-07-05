0fa555d6b42b0753653a548b5fe4c7fa7df7726b
wissemnaddari <168022098+wissemnaddari@users.noreply.github.com>
2025-07-05 14:56:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = 'uAhfHg9zcm9gHk3'  
+VERIFY_TOKEN = 'uAhfHg9zcm9gHk3'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST", "HEAD"])
 def webhook():
-    if request.method == 'GET':
+    if request.method == "HEAD":
+        return '', 200
+
+    if request.method == "GET":
         mode = request.args.get('hub.mode')
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
@@ -16,11 +19,10 @@ def webhook():
         else:
             return 'Verification token mismatch', 403
 
-    if request.method == 'POST':
-        # هنا تعالج الميساجات اللي تبعثهم Facebook
+    if request.method == "POST":
         data = request.get_json()
         print("Webhook received:", data)
         return 'EVENT_RECEIVED', 200
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
